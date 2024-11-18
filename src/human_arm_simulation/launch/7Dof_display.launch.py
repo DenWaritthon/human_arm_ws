5cf1c61c71d8aa4c5ae69c3c53574be41bde0520
ThepokKung <pok23250@gmail.com>
2024-11-18 23:19:03 +0700

    Add urdf file to main project but Axis don't sure.

---
+++ src/human_arm_simulation/launch/7Dof_display.launch.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+import xacro    
+    
+def generate_launch_description():
+    
+    pkg = get_package_share_directory('human_arm_simulation')
+    rviz_path = os.path.join(pkg,'config','display.rviz')
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz',
+        arguments=['-d', rviz_path],
+        output='screen')
+    
+    path_description = os.path.join(pkg,'robot','visual','7DOF_robot.xacro')
+    robot_desc_xml = xacro.process_file(path_description).toxml()
+    
+    parameters = [{'robot_description':robot_desc_xml}]
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                  executable='robot_state_publisher',
+                                  output='screen',
+                                  parameters=parameters
+    )
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
+
+    launch_description = LaunchDescription()
+    
+    launch_description.add_action(rviz)
+    launch_description.add_action(robot_state_publisher)
+    launch_description.add_action(joint_state_publisher_gui)
+    
+    return launch_description
